b3028a6c3ecf50597edf8f75d1396573bed1cf39
Johnny G.G <jgreengroves@gmail.com>
2026-05-07 17:39:39 -0400

    Add files via upload

---
+++ js/data/products.js
@@ -0,0 +1,113 @@
+export const products = [ 
+    {
+        id: 1,
+        name: "Apple iphone 17 Lavender - 256",
+        price: 829.99,
+        description: "iPhone 17. A faster way to manage your workday. Brighter 6.3-inch ProMotion display,1 durable Ceramic Shield 2, faster A19 chip, and all-day battery life.",
+        image: "https://cdn.tmobile.com/images/png/products/phones/Apple-iPhone-17-Black/250x270_1.png"
+    },
+
+    {
+        id: 2,
+        name: "Apple iphone 17 sage green - 256GB",
+        price: 829.99,
+        description: "iPhone 17. A faster way to manage your workday. Brighter 6.3-inch ProMotion display,1 durable Ceramic Shield 2, faster A19 chip, and all-day battery life.",
+        image: "https://cdn.tmobile.com/images/png/products/phones/Apple-iPhone-17-Sage/250x270_1.png"
+    },
+
+    {
+        id: 3,
+        name: "Apple iphone 17 white - 256GB",
+        price: 829.99,
+        description: "iPhone 17. A faster way to manage your workday. Brighter 6.3-inch ProMotion display,1 durable Ceramic Shield 2, faster A19 chip, and all-day battery life.",
+        image: "https://cdn.tmobile.com/images/png/products/phones/Apple-iPhone-17-White/250x270_1.png"
+    },
+
+    {
+        id: 4,
+        name: "Apple iphone 17 black - 256GB",
+        price: 829.99,
+        description: "iPhone 17. A faster way to manage your workday. Brighter 6.3-inch ProMotion display,1 durable Ceramic Shield 2, faster A19 chip, and all-day battery life.",
+        image: "https://cdn.tmobile.com/images/png/products/phones/Apple-iPhone-17-Black/250x270_1.png"
+    },
+
+    
+    {
+        id: 5,
+        name: "Apple ipad blue - 256GB",
+        price: 599.99,
+        description: "iPad. A faster way to manage your workday. Brighter 10.9-inch Liquid Retina display,1 durable Ceramic Shield 2, faster A14 chip, and all-day battery life.",
+        image: "https://cdn.tmobile.com/images/png/products/phones/Apple-iPad-Blue/250x270_1.png"
+    },
+    {
+        id: 6,
+        name: "Apple ipad silver - 512GB",
+        price: 799.99,
+        description: "iPad. A faster way to manage your workday. Brighter 10.9-inch Liquid Retina display,1 durable Ceramic Shield 2, faster A19 chip, and all-day battery life.",
+        image: "https://cdn.tmobile.com/images/png/products/phones/Apple-iPad-Silver/250x270_1.png"
+    },
+    {
+        id: 7,
+        name: "Apple ipad pink - 512GB",
+        price: 799.99,
+        description: "iPad. A faster way to manage your workday. Brighter 10.9-inch Liquid Retina display,1 durable Ceramic Shield 2, faster A19 chip, and all-day battery life.",
+        image: "https://cdn.tmobile.com/images/png/products/phones/Apple-iPad-Pink/250x270_1.png"
+    },
+    {
+        id: 8,
+        name: "Apple iphone Air space black - 256",
+        price: 999.99,
+        description: "iPhone 17. A faster way to manage your workday. Brighter 6.3-inch ProMotion display,1 durable Ceramic Shield 2, faster A19 chip, and all-day battery life.",
+        image: "https://cdn.tmobile.com/images/png/products/phones/Apple-iPhone-Air-Space-Black/250x270_1.png"
+    },
+    {
+        id: 9,
+        name: "Green Imac - Apple education store - 512GB",
+        price: 1599.99,
+        description: "iMac. A faster way to manage your workday. Brighter 24-inch Retina display,1 durable Ceramic Shield 2, faster A19 chip, and all-day battery life.",
+        image: "https://cdn.tmobile.com/images/png/products/phones/Apple-iMac-Green/250x270_1.png"
+    },
+    {
+        id: 10,
+        name: "Blue Imac - Apple education store - 512GB",
+        price: 1599.99,
+        description: "iMac. A faster way to manage your workday. Brighter 24-inch Retina display,1 durable Ceramic Shield 2, faster A19 chip, and all-day battery life.",
+        image: "https://cdn.tmobile.com/images/png/products/phones/Apple-iMac-Blue/250x270_1.png"
+    },
+    {
+        id: 11,
+        name: "Pink Imac - Apple education store - 512GB",
+        price: 1599.99,
+        description: "iMac. A faster way to manage your workday. Brighter 24-inch Retina display,1 durable Ceramic Shield 2, faster A19 chip, and all-day battery life.",
+        image: "https://cdn.tmobile.com/images/png/products/phones/Apple-iMac-Pink/250x270_1.png"
+    },
+    {
+        id: 12,
+        name: "Apple iphone 13 pro max sierra - blue - 256GB",
+        price: 456.99,
+        description: "iPhone 13 pro max sierra - blue - 256. A faster way to manage your workday. Brighter 6.3-inch ProMotion display,1 durable Ceramic Shield 2, faster A19 chip, and all-day battery life.",
+        image: "https://cdn.tmobile.com/images/png/products/phones/Apple-iPhone-13-Pro-Max-Sierra-Blue-Lavender/250x270_1.png"
+    },
+    {
+        id: 13,
+        name: "Apple iphone 13 pro max graphite - 256GB",
+        price: 456.99,
+        description: "iPhone 13 pro max graphite - 256GB. A faster way to manage your workday. Brighter 6.3-inch ProMotion display,1 durable Ceramic Shield 2, faster A19 chip, and all-day battery life.",
+        image: "https://cdn.tmobile.com/images/png/products/phones/Apple-iPhone-13-Pro-Max-Graphite/250x270_1.png"
+    },
+    {
+        id: 14,
+        name: "Apple iphone 13 pro max gold - 256GB",
+        price: 456.99,
+        description: "iPhone 13 pro max gold - 256GB. A faster way to manage your workday. Brighter 6.3-inch ProMotion display,1 durable Ceramic Shield 2, faster A19 chip, and all-day battery life.",
+        image: "https://cdn.tmobile.com/images/png/products/phones/Apple-iPhone-13-Pro-Max-Gold/250x270_1.png"
+    },
+    {
+        id: 15,
+        name: "Apple iphone 13 pro max alpine green - 256GB",
+        price: 456.99,
+        description: "iPhone 13 pro max alpine green - 256GB. A faster way to manage your workday. Brighter 6.3-inch ProMotion display,1 durable Ceramic Shield 2, faster A19 chip, and all-day battery life.",
+        image: "https://www.plug.tech/cdn/shop/products/13ProMaxSilver_Dual_1_3dfcee82-add6-416f-bae6-e03b8a1947e5.png?v=1667411359&width=823"
+    }
+];
+
