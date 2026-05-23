1188763bb76be397c133f312c0b6ef8c31cdfd7d
Johnathan <jgreengroves@gmail.com>
2026-05-23 12:17:08 -0400

    Fixed broken image links in product.js

--- src/data/products.js
+++ src/data/products.js
@@ -1,7 +1,7 @@
 export const products = [ 
     {
         id: 1,
-        name: "Apple iphone 17 Lavender - 256",
+        name: "Apple iphone 17 Black - 256GB",
         price: 829.99,
         description: "iPhone 17. A faster way to manage your workday. Brighter 6.3-inch ProMotion display,1 durable Ceramic Shield 2, faster A19 chip, and all-day battery life.",
         image: "https://cdn.tmobile.com/images/png/products/phones/Apple-iPhone-17-Black/250x270_1.png"
@@ -37,21 +37,21 @@ export const products = [
         name: "Apple ipad blue - 256GB",
         price: 599.99,
         description: "iPad. A faster way to manage your workday. Brighter 10.9-inch Liquid Retina display,1 durable Ceramic Shield 2, faster A14 chip, and all-day battery life.",
-        image: "https://cdn.tmobile.com/images/png/products/phones/Apple-iPad-Blue/250x270_1.png"
+        image: "https://t-mobile.scene7.com/is/image/Tmusprod/Apple-iPad-A16-Blue-frontimage?fmt=png-alpha&qlt=85,0&resMode=sharp2&op_usm=1.75,0.3,2,0&dpr=off"
     },
     {
         id: 6,
         name: "Apple ipad silver - 512GB",
         price: 799.99,
         description: "iPad. A faster way to manage your workday. Brighter 10.9-inch Liquid Retina display,1 durable Ceramic Shield 2, faster A19 chip, and all-day battery life.",
-        image: "https://cdn.tmobile.com/images/png/products/phones/Apple-iPad-Silver/250x270_1.png"
+        image: "https://t-mobile.scene7.com/is/image/Tmusprod/Apple-iPad-A16-Silver-frontimage?fmt=png-alpha&qlt=85,0&resMode=sharp2&op_usm=1.75,0.3,2,0&dpr=off"
     },
     {
         id: 7,
         name: "Apple ipad pink - 512GB",
         price: 799.99,
         description: "iPad. A faster way to manage your workday. Brighter 10.9-inch Liquid Retina display,1 durable Ceramic Shield 2, faster A19 chip, and all-day battery life.",
-        image: "https://cdn.tmobile.com/images/png/products/phones/Apple-iPad-Pink/250x270_1.png"
+        image: "https://t-mobile.scene7.com/is/image/Tmusprod/Apple-iPad-A16-Pink-frontimage?fmt=png-alpha&qlt=85,0&resMode=sharp2&op_usm=1.75,0.3,2,0&dpr=off"
     },
     {
         id: 8,
@@ -64,29 +64,29 @@ export const products = [
         id: 9,
         name: "Green Imac - Apple education store - 512GB",
         price: 1599.99,
-        description: "iMac. A faster way to manage your workday. Brighter 24-inch Retina display,1 durable Ceramic Shield 2, faster A19 chip, and all-day battery life.",
-        image: "https://cdn.tmobile.com/images/png/products/phones/Apple-iMac-Green/250x270_1.png"
+        description: "An all-in-one desktop, iMac is strikingly thin, comes in seven vibrant colors, and complements any room",
+        image: "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/imac-digitalmat-gallery-4-202410?wid=728&hei=666&fmt=png-alpha&.v=OURsbmhReXErU1E1S050OGsrUXJyVWVwdzkwem1sS3FwUzFXRXFzeTFaVVRDMFgyWE5CaGc1dHIrSkVKb1NNV1M0TjRWdzF2UjRGVEY0c3dBQVZ6VFFKbUZQdXArWmtLQTB3T3RIL1RudmM"
     },
     {
         id: 10,
         name: "Blue Imac - Apple education store - 512GB",
         price: 1599.99,
-        description: "iMac. A faster way to manage your workday. Brighter 24-inch Retina display,1 durable Ceramic Shield 2, faster A19 chip, and all-day battery life.",
-        image: "https://cdn.tmobile.com/images/png/products/phones/Apple-iMac-Blue/250x270_1.png"
+        description: "An all-in-one desktop, iMac is strikingly thin, comes in seven vibrant colors, and complements any room",
+        image: "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/imac-digitalmat-gallery-5-202410?wid=728&hei=666&fmt=png-alpha&.v=OURsbmhReXErU1E1S050OGsrUXJyWHp1UmN1YnJXTmNwUFFnZFFIWEkva1RDMFgyWE5CaGc1dHIrSkVKb1NNV1M0TjRWdzF2UjRGVEY0c3dBQVZ6VGFrYUhTeDhEVitTUittemluVWpwVkE"
     },
     {
         id: 11,
         name: "Pink Imac - Apple education store - 512GB",
         price: 1599.99,
-        description: "iMac. A faster way to manage your workday. Brighter 24-inch Retina display,1 durable Ceramic Shield 2, faster A19 chip, and all-day battery life.",
-        image: "https://cdn.tmobile.com/images/png/products/phones/Apple-iMac-Pink/250x270_1.png"
+        description: "An all-in-one desktop, iMac is strikingly thin, comes in seven vibrant colors, and complements any room",
+        image: "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/imac-digitalmat-gallery-3-202410?wid=728&hei=666&fmt=png-alpha&.v=OURsbmhReXErU1E1S050OGsrUXJyUWM2UW16ai91VTE3Q2IycGdWVjFPUVRDMFgyWE5CaGc1dHIrSkVKb1NNV1M0TjRWdzF2UjRGVEY0c3dBQVZ6VFl4SFM0MitBYU9jcDdGMkxLb1RlZWs"
     },
     {
         id: 12,
-        name: "Apple iphone 13 pro max sierra - blue - 256GB",
-        price: 456.99,
-        description: "iPhone 13 pro max sierra - blue - 256. A faster way to manage your workday. Brighter 6.3-inch ProMotion display,1 durable Ceramic Shield 2, faster A19 chip, and all-day battery life.",
-        image: "https://cdn.tmobile.com/images/png/products/phones/Apple-iPhone-13-Pro-Max-Sierra-Blue-Lavender/250x270_1.png"
+        name: "Apple iphone 17 Pro Max Cosmic - Orange - 1TB",
+        price: 1045.99,
+        description: "Innovation design for ultimate performance and battery life. ",
+        image: "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-17-pro-model-unselect-gallery-2-202509_GEO_US?wid=5120&hei=2880&fmt=webp&qlt=90&.v=dU9qRExIQUlQTzVKeDd1V1dtUE1MUWFRQXQ2R0JQTk5udUZxTkR3ZVlpTEJBSVhDREVhQVF4eThVb2E3Y2VibUlYUWYrQkRLNitCbE9QRVRqNHErMkE3b3pFWnhZZ2g0M0pRR0pEdHVSRUVtYkFqYmVJbENIK1gycDVvVjJtTEZkWHlyQUZHM0VoZTBZR3diWjJXVERn&traceId=1"
     },
     {
         id: 13,
@@ -107,7 +107,7 @@ export const products = [
         name: "Apple iphone 13 pro max alpine green - 256GB",
         price: 456.99,
         description: "iPhone 13 pro max alpine green - 256GB. A faster way to manage your workday. Brighter 6.3-inch ProMotion display,1 durable Ceramic Shield 2, faster A19 chip, and all-day battery life.",
-        image: "https://www.plug.tech/cdn/shop/products/13ProMaxSilver_Dual_1_3dfcee82-add6-416f-bae6-e03b8a1947e5.png?v=1667411359&width=823"
+        image: "https://cdn.tmobile.com/images/png/products/phones/Apple-iPhone-13-Pro-Max-Alpine-Green/250x270_1.png"
     }
 ];
 
